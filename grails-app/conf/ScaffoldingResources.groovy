9a6cd290245b588aef6d16b2534e525d65fc320e
Rafael <rv@manoderecha.mx>
2013-01-20 04:03:02 -0600

    Se agrega el jumbotron a los estilos.

--- grails-app/conf/ScaffoldingResources.groovy
+++ grails-app/conf/ScaffoldingResources.groovy
@@ -3,5 +3,6 @@ modules = {
 		resource url: [dir: 'less', file: 'custom-bootstrap.less'], attrs:[rel: "stylesheet/less", type:'css']
 		resource url:[plugin: 'twitter-bootstrap', dir: 'less', file: 'responsive.less'],attrs:[rel:   "stylesheet/less", type:'css'], disposition: 'head'
 		resource url: 'css/scaffolding.css'
+		resource url: 'css/jumbotron.css'
 	}
 }
